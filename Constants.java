6b52234193f0210b630edc39dddee28eec8c0dbf
zdttt <158805709+zdttt@users.noreply.github.com>
2024-02-28 16:14:44 +0800

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,10 @@
+package org.joychou.config;
+
+public class Constants {
+
+    private Constants() {
+    }
+
+    public static final String REMEMBER_ME_COOKIE = "rememberMe";
+    public static final String ERROR_PAGE = "https://test.joychou.org/error1.html";
+}
